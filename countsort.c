4abea40fc8560f03790c539365a3dd8a2564a52c
iFlameing <ialokkumarsingh0@gmail.com>
2019-04-29 19:22:11 +0530

    fully implemented countsort.c

--- countsort.c
+++ countsort.c
@@ -1,38 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main() {
-    int i,j,k,n;
-    scanf("%d", &n);
-    int a[n];
-    for(i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
-    count_sort(a,n);
-    for(i=0;i<n;i++){
-        printf("%d", a[i]);
-    }
-}
 
 void count_sort(int a[], int n) {
 
     int max,i,j;
     int b[n];
-    max= a[0];
+    max = a[0];
     for(i=0;i<n;i++){
         if(a[i]>max) {
             max=a[i];
         }
     }
-    int count[max] = {0}; 
+
+    int count[max+1];
+    for( i=0;i<=max;i++) {
+        count[i] = 0;
+    }
     for(i=0;i<n;i++) {
-        count[a[i]]++
+        count[a[i]]++;
     }
 
-    for(i=1;i<n;i++){
+    for(i=1;i<max+1;i++){
         count[i]+= count[i-1];
     }
-
+    
     for(i=0;i<n;i++){
         b[count[a[i]]-1] = a[i];
         count[a[i]]--;
@@ -43,3 +35,20 @@ void count_sort(int a[], int n) {
     }
 
 }
+
+
+int main() {
+    int i,j,k,n;
+    scanf("%d", &n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    count_sort(a,n);
+    printf("sorted array is below\n");
+    for(i=0;i<n;i++){
+        printf("%d\n", a[i]);
+    }
+}
+
